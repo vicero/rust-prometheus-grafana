a03a90f1593b858b7afdb43a35d6e291c14b6a87
James Kessler <james.kessler@autostoresystem.com>
2025-05-16 11:07:50 -0600

    set up alert manager

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ mod metrics_middleware;
 mod metrics_router;
 
 use axum::{middleware, response::Html, routing::get, Router};
+use axum::extract::Path;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
     let app = Router::new()
         .route("/", get(handler))
+        .route("/delay/{milliseconds}", get(delay))
         // introduce the metrics middleware before adding metrics, since we
         // want to exclude the /metrics route from prometheus
         .route_layer(middleware::from_fn(metrics_middleware::track_metrics))
@@ -31,4 +33,13 @@ async fn main() {
 
 async fn handler() -> Html<&'static str> {
     Html("<h1>Hello, World!</h1>")
+}
+
+async fn delay(Path(milliseconds): Path<i16>) -> Html <String> {
+    {
+        // execute the delay specified by the path parameter
+        tokio::time::sleep(std::time::Duration::from_millis(milliseconds as u64)).await;
+        // return a simple HTML response
+        Html(format!("<p>Delayed for <em>{milliseconds}</em> milliseconds</p>"))
+    }
 }
